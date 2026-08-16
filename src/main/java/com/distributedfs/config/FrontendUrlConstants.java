ea0918acd7b54210c65b2cc317c710ba4fa77fed
Arihant Jain <aunujain53@gmail.com>
2026-08-16 20:37:26 +0530

    feat: add 127.0.0.1 to default CORS allowed origin patterns for local testing

--- src/main/java/com/distributedfs/config/FrontendUrlConstants.java
+++ src/main/java/com/distributedfs/config/FrontendUrlConstants.java
@@ -5,9 +5,11 @@ import java.util.List;
 public final class FrontendUrlConstants {
 
     public static final String LOCALHOST_FRONTEND_ORIGIN_PATTERN = "http://localhost:*";
+    public static final String TEST_FRONTEND_ORIGIN_PATTERN = "http://127.0.0.1:*";
     public static final String DUCKDNS_FRONTEND_ORIGIN = "http://dfs-ui.duckdns.org";
     public static final List<String> DEFAULT_CORS_ALLOWED_ORIGIN_PATTERNS = List.of(
         LOCALHOST_FRONTEND_ORIGIN_PATTERN,
+        TEST_FRONTEND_ORIGIN_PATTERN,
         DUCKDNS_FRONTEND_ORIGIN
     );
 
